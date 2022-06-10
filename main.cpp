e5d00897626a9b2bf9d78cf258020a489283f78d
jsrankin-com <89367845+jsrankin-com@users.noreply.github.com>
2022-06-10 04:42:17 -0400

    Update main.cpp
    Optimized layout of the code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // ************************EIGHTY CHARACTER CODING AREA*************************// LANDSCAPE COMMENT SECTION LENGTH
 /*
                _______.     main.cpp
-   ______     | .   . |\      Driver program to test dice_rng.cpp and 
+   ______     | .   . |\      Driver program to test dice_rng.cpp and
   /     /\    |   .   |.\   dice_rng.hpp. Auto keyword since C++11.
  /  '  /  \   | .   . |.'|
 /_____/. . \  |_______|.'|
@@ -13,89 +13,71 @@
 #include "dice_rng.h"
 #include <iostream>
 
-template <class T>
-T ReturnSmaller(T lhs, T rhs)
-{
-    return lhs < rhs ? lhs : rhs;
-}
-
 int main()
 {
     DiceRNG d;
 
-    std::cout << "One roll:\t" << d() << std::endl;
-    std::cout << "One roll:\t" << d.Roll() << std::endl;
+    std::cout << "One roll:\t" << d() << '\n';
+    std::cout << "One roll:\t" << d.Roll() << '\n';
 
     std::cout << "Five rolls:\t";
     for (auto x : d(5))
     {
         std::cout << x << ' ';
     }
-    std::cout << std::endl;
+    std::cout << '\n';
 
     std::cout << "Five rolls:\t";
     for (auto x : d.Roll(5))
     {
         std::cout << x << ' ';
     }
-    std::cout << std::endl;
+    std::cout << '\n';
 
-    std::cout << "Risk test:" << std::endl;
-    std::vector<int> attRoles;
-    std::vector<int> defRoles;
+    std::cout << "Risk test:" << '\n';
 
     int attacking_army = 50;
     int defending_army = 50;
 
     while (0 < attacking_army && 0 < defending_army)
     {
-        //Set attacking dice 3 or less
-        if (attacking_army < 3)
-        {
-            attRoles = d.Roll(attacking_army);
-        }
-        else
-        {
-            attRoles = d.Roll(3);
-        }
+        //Set attacking dice to 3 or less
+        std::vector<int> att_rolls = (attacking_army < 3) 
+            ? d.Roll(attacking_army) 
+            : d.Roll(3);
 
-        //Set defending dice 2 or 1
-        if (defending_army == 1)
-        {
-            defRoles = d.Roll(1);
-        }
-        else {
-            defRoles = d.Roll(2);
-        }
+        //Set defending dice to 2 or 1
+        std::vector<int> def_rolls = (defending_army == 1) 
+            ? d.Roll(defending_army)
+            : d.Roll(2);
 
         //Sort to compare best Rolls
-        std::sort(attRoles.begin(), attRoles.end(), std::greater<>());
-        std::sort(defRoles.begin(), defRoles.end(), std::greater<>());
+        std::sort(att_rolls.begin(), att_rolls.end(), std::greater<>());
+        std::sort(def_rolls.begin(), def_rolls.end(), std::greater<>());
 
-        int iterations = ReturnSmaller(attRoles.size(), defRoles.size());
+        int iterations = (att_rolls.size() < def_rolls.size())
+            ? att_rolls.size()
+            : def_rolls.size();
+
         for (int i = 0; i < iterations; ++i)
         {
-            //Defender wins if Rolls are equal or higher
-            if (defRoles[i] < attRoles[i])
+            //Defender wins if rolls are equal
+            if (def_rolls[i] < att_rolls[i])
             {
                 --defending_army;
-                std::cout << "ATTACKER WON" << std::endl;
+                std::cout << "ATTACKER WON" << '\n';
             }
             else {
                 --attacking_army;
-                std::cout << "ATTACKER LOST" << std::endl;
+                std::cout << "ATTACKER LOST" << '\n';
             }
         }
     }
 
-    if (attacking_army < defending_army)
-    {
-        std::cout << "ATTACKER LOST THE BATTLE\n";
-    }
-    else
-    {
-        std::cout << "ATTACKER WON THE BATTLE\n";
-    }
+    (attacking_army < defending_army)
+        ? std::cout << "\nATTACKER LOST THE BATTLE\n"
+        : std::cout << "\nATTACKER WON THE BATTLE\n";
+
     std::cout << "ATTACKING ARMY: " << attacking_army
         << "\nDEFENDING ARMY: " << defending_army << std::endl;
 
